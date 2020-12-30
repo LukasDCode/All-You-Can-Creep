bc1e30321b08c95139064852bc87643dc944fe8a
Lu <apphunter.ah@gmail.com>
2020-12-30 14:14:30 +0100

    fixed indentation and added examples to visualize/main.py

--- project/src/worm/worm.py
+++ project/src/worm/worm.py
@@ -28,120 +28,117 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
         self.result_base_name = config.result
         self.scale = config.scale
 
-
-    def run(self,worker_id, params):
+    def run(self, worker_id, params):
         (rewards, losses_dicts) = self.run_with_params(
-          worker_id=worker_id,
-          params=params,
+            worker_id=worker_id,
+            params=params,
         )
         result_dump = {
-          "algorithm": "a2c",
-          "params" : params,
-          "measures" : {"rewards" : rewards, **losses_dicts}
+            "algorithm": "a2c",
+            "params": params,
+            "measures": {"rewards": rewards, **losses_dicts}
         }
-        with open(self.result_base_name + str(uuid4()) +".json",'w+') as file:
-          json.dump(result_dump, file, indent=2)
+        with open(self.result_base_name + str(uuid4()) + ".json", 'w+') as file:
+            json.dump(result_dump, file, indent=2)
         return rewards
-    
+
     def param_dict(self):
         return [
-          ("alpha", 0.001,0.001),
-          ("gamma", 0.99,1.),
+            ("alpha", 0.001, 0.001),
+            ("gamma", 0.99, 1.),
         ]
 
     def episode(self, env, agent, nr_episode=0):
-      state = env.reset()
+        state = env.reset()
 
-      undiscounted_return = 0
-      done = False
-      time_step = 0
-      losses_dict= None
-      while not done:
-          if self.render_env:
-            env.render()
-          # 1. Select action according to policy
-          action = agent.policy(state)
-          # 2. Execute selected action
-          next_state, reward, done, _ = env.step(action)
+        undiscounted_return = 0
+        done = False
+        time_step = 0
+        losses_dict = None
+        while not done:
+            if self.render_env:
+                env.render()
+            # 1. Select action according to policy
+            action = agent.policy(state)
+            # 2. Execute selected action
+            next_state, reward, done, _ = env.step(action)
             # 3. Integrate new experience into agent
-          losses_dict = agent.update(state, action, reward, next_state, done)
-          state = next_state
-          undiscounted_return += reward
-          time_step += 1
-      print(nr_episode, ":", undiscounted_return)
-      return undiscounted_return, losses_dict
-
+            losses_dict = agent.update(state, action, reward, next_state, done)
+            state = next_state
+            undiscounted_return += reward
+            time_step += 1
+        print(nr_episode, ":", undiscounted_return)
+        return undiscounted_return, losses_dict
 
     def run_with_params(self, worker_id, params,):
-      params = params.copy()
+        params = params.copy()
 
-      # Domain setup
-      # Environment
-      channel = EngineConfigurationChannel()
-      channel.set_configuration_parameters(time_scale = self.scale)
+        # Domain setup
+        # Environment
+        channel = EngineConfigurationChannel()
+        channel.set_configuration_parameters(time_scale=self.scale)
 
-      unity_env = UnityEnvironment(
-        file_name="Unity/worm_single_environment.x86_64" if "linux" in platform else "Unity",
-        worker_id=worker_id,
-        no_graphics=not self.render_env,
-        side_channels=[channel],
+        unity_env = UnityEnvironment(
+            file_name="Unity/worm_single_environment.x86_64" if "linux" in platform else "Unity",
+            worker_id=worker_id,
+            no_graphics=not self.render_env,
+            side_channels=[channel],
         )
 
-      env = UnityToGymWrapper(unity_env)
-      params["nr_input_features"] = env.observation_space.shape[0] # 64
-      params["env"] = env
-      params["nr_actions"] = env.action_space.shape[0] # 9
-      params["lower_bound"] = env.action_space.low
-      params["upper_bound"] = env.action_space.high
-      params["type"] = env.action_space.dtype
+        env = UnityToGymWrapper(unity_env)
+        params["nr_input_features"] = env.observation_space.shape[0]  # 64
+        params["env"] = env
+        params["nr_actions"] = env.action_space.shape[0]  # 9
+        params["lower_bound"] = env.action_space.low
+        params["upper_bound"] = env.action_space.high
+        params["type"] = env.action_space.dtype
 
-      # Agent setup
-      agent = A2CLearner(params)
-      # train
-      results = [
-        self.episode(env, agent, nr_episode=i,)
-        for i in range(self.training_episodes)]
+        # Agent setup
+        agent = A2CLearner(params)
+        # train
+        results = [
+            self.episode(env, agent, nr_episode=i,)
+            for i in range(self.training_episodes)]
 
-      #not needed anymore
-      unity_env.close()
-      
-      returns, losses_dicts = zip(*results)
-      def flatmap(func, *iterable):
-        return itertools.chain.from_iterable(map(func, *iterable))
-      squeezed_losses_dicts = defaultdict(list)
-      #for key,value in [entry for d in losses_dicts for entry in d.items()]:
-      for key,value in flatmap(lambda d: d.items(), losses_dicts):
-          squeezed_losses_dicts[key].append(value)
+        # not needed anymore
+        unity_env.close()
 
-      return returns, squeezed_losses_dicts 
-          
+        returns, losses_dicts = zip(*results)
 
+        def flatmap(func, *iterable):
+            return itertools.chain.from_iterable(map(func, *iterable))
+        squeezed_losses_dicts = defaultdict(list)
+        # for key,value in [entry for d in losses_dicts for entry in d.items()]:
+        for key, value in flatmap(lambda d: d.items(), losses_dicts):
+            squeezed_losses_dicts[key].append(value)
 
+        return returns, squeezed_losses_dicts
 
 
 def parse_config():
-  parser = argparse.ArgumentParser(description='Run worms with hyper params')
-  parser.add_argument('-a','--alpha', type=float, default=0.001, help='the learning rate')
-  parser.add_argument('-g','--gamma', type=float, default=0.99 , help='the discount factor for rewards')
-  parser.add_argument('-n','--episodes', type=int, default=2000, help='training episodes')
-  parser.add_argument('-v', '--visualize', type=bool, default=False, help='call env.render')  
-  parser.add_argument('-s', '--scale', type=float, default=1.0, help='simulation speed scaling')
-  parser.add_argument('-r', '--result', type=str, default="result", help='file base name to save results into')
-  parser.add_argument('-p', '--parallel',type=int, default=1, help='level on parallization')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(description='Run worms with hyper params')
+    parser.add_argument('-a', '--alpha', type=float, default=0.001, help='the learning rate')
+    parser.add_argument('-g', '--gamma', type=float, default=0.99, help='the discount factor for rewards')
+    parser.add_argument('-n', '--episodes', type=int, default=2000, help='training episodes')
+    parser.add_argument('-v', '--visualize', type=bool, default=False, help='call env.render')
+    parser.add_argument('-s', '--scale', type=float, default=10.0, help='simulation speed scaling')
+    parser.add_argument('-r', '--result', type=str, default="result", help='file base name to save results into')
+    parser.add_argument('-p', '--parallel', type=int, default=1, help='level on parallelization')
+    return parser.parse_args()
 
 
 def main():
-  config = parse_config()
-  print("Run with {}", str(config))
-  domain = WormDomainAdaptor(config)
-  with Executor(tasks_in_parallel=config.parallel, on_slurm=False, domain=domain) as executor:
-    # Hyperparameters
-    params = {}
-    params["gamma"] = config.gamma
-    params["alpha"] = config.alpha
-    future = executor.submit_task(params)
-    print(future.get())
+    config = parse_config()
+    print("Run with {}", str(config))
+    domain = WormDomainAdaptor(config)
+    with Executor(tasks_in_parallel=config.parallel, on_slurm=False, domain=domain) as executor:
+        # Hyperparameters
+        params = {}
+        params["gamma"] = config.gamma
+        params["alpha"] = config.alpha
+        future = executor.submit_task(params)
+        print(future.get())
+
 
 if __name__ == "__main__":
-  main()
+    main()
