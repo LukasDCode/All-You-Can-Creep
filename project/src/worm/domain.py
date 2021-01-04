483f757f48b016f5101dea0db39ee9ece4f184fa
Lu <apphunter.ah@gmail.com>
2021-01-04 19:17:35 +0100

    distance from worm to target is working and being written to json

--- project/src/worm/domain.py
+++ project/src/worm/domain.py
@@ -23,8 +23,12 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
 
     @staticmethod
     def add_parse_args(parser):
-      parser.add_argument('-n','--episodes', type=int, default=2000, help='training episodes')
-      parser.add_argument('-v', '--visualize', type=bool, default=False, help='call env.render')  
+      #parser.add_argument('-n','--episodes', type=int, default=2000, help='training episodes')
+      #parser.add_argument('-v', '--visualize', type=bool, default=False, help='call env.render')  
+      #parser.add_argument('-t', '--time_scale', type=float, default=20, help='simulation speed scaling, higher values make physics less precise')
+      #parser.add_argument('-r', '--result', type=str, default="result", help='file base name to save results into')
+      parser.add_argument('-n','--episodes', type=int, default=3, help='training episodes')
+      parser.add_argument('-v', '--visualize', type=bool, default=True, help='call env.render')  
       parser.add_argument('-t', '--time_scale', type=float, default=20, help='simulation speed scaling, higher values make physics less precise')
       parser.add_argument('-r', '--result', type=str, default="result", help='file base name to save results into')
       return parser
@@ -36,16 +40,17 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
         self.training_episodes = config.episodes
         self.result_base_name = config.result
         self.scale = config.time_scale
+        # self.distances = []
 
     def run(self, worker_id, params):
-        (rewards, losses_dicts) = self.run_with_params(
+        (rewards, measurements_dicts) = self.run_with_params(
             worker_id=worker_id,
             params=params,
         )
         result_dump = {
             "algorithm": "a2c",
             "params": params,
-            "measures": {"rewards": rewards, **losses_dicts}
+            "measures": {"rewards": rewards, **measurements_dicts}
         }
         result_path = Path(self.result_base_name + str(uuid4()) + ".json")
         result_path.parent.mkdir(parents=True, exist_ok=True)
@@ -73,12 +78,38 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
             # 1. Select action according to policy
             action = agent.policy(state)
             # 2. Execute selected action
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, _ = env.step(action) # _ = decision_steps but are not used here
             # 3. Integrate new experience into agent
             losses_dict = agent.update(state, action, reward, next_state, done)
             state = next_state
             undiscounted_return += reward
             time_step += 1
+
+            # for this to work you have to replace the "_" with "decision_steps" at env.step(action)
+            # decision_step_distance = decision_steps['step'].obs[0][0][4]
+            # self.distances.append(decision_step_distance)
+
+
+
+        """
+        # for this to work, you have to pass on the unity_env as a parameter
+        group_name = list(unity_env.behavior_specs.keys())[0]  # Get the first group_name
+        # group_spec = unity_env.behavior_specs[group_name]
+
+        # We send data to Unity : A string with the number of Agent at each
+        _, terminal_steps = unity_env.get_steps(group_name) # _ = decision_steps # not used here
+
+        terminal_step = terminal_steps[0].obs
+        terminal_step_distance = terminal_step[0][4]
+        self.distances.append(terminal_step_distance)
+
+        min_distance = min(self.distances)
+        max_distance = max(self.distances)
+        avg_distance = 0 if len(self.distances) == 0 else sum(self.distances)/len(self.distances)
+        last_distance = terminal_step_distance
+        """
+
+
         print(nr_episode, ":", undiscounted_return)
         return undiscounted_return, losses_dict
 
@@ -97,6 +128,7 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
             no_graphics=not self.render_env,
             side_channels=[channel],
         )
+        unity_env.reset()
 
         env = UnityToGymWrapper(unity_env)
         params["nr_input_features"] = env.observation_space.shape[0]  # 64
@@ -113,15 +145,18 @@ class WormDomainAdaptor(DomainTrainingAdaptor):
             self.episode(env, agent, nr_episode=i,)
             for i in range(self.training_episodes)]
 
+
+
+
         # not needed anymore
         unity_env.close()
 
-        returns, losses_dicts = zip(*results)
+        returns, measurements_dicts = zip(*results)
 
         def flatmap(func, *iterable):
             return itertools.chain.from_iterable(map(func, *iterable))
-        squeezed_losses_dicts = defaultdict(list)
-        # for key,value in [entry for d in losses_dicts for entry in d.items()]:
-        for key, value in flatmap(lambda d: d.items(), losses_dicts):
-            squeezed_losses_dicts[key].append(value)
-        return returns, squeezed_losses_dicts
+        squeezed_measurements_dicts = defaultdict(list)
+        # for key,value in [entry for d in measurements_dicts for entry in d.items()]:
+        for key, value in flatmap(lambda d: d.items(), measurements_dicts):
+            squeezed_measurements_dicts[key].append(value)
+        return returns, squeezed_measurements_dicts
