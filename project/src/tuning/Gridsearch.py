d26fc16aa6ce25af237c59ba12832334bd475567
= <sofie.henghuber@campus.lmu.de>
2021-01-04 14:56:47 +0100

    hyperparams grid

--- project/src/tuning/Gridsearch.py
+++ project/src/tuning/Gridsearch.py
@@ -8,9 +8,9 @@ class Gridsearch():
     def run(self):
 
         hyperparams = {
-                "alpha":[0.001],
-                "gamma":[0.9,1],
-                "entropy":[1e-323, 1e-4]
+                "alpha":[0.001,0.002,0.004,0.008,0.0016,0.0032,0.0064, 0.0128,0.0256,0.0512, 0.1024,0.2048],
+                "gamma":[0.999,0.9995, 0.9999,0.9995, 0.99999,1.],
+                "entropy":[1e-8, 1e-7, 1e-6, 1e-5, 1e-4]
             }
 
         for i in hyperparams["alpha"]:
