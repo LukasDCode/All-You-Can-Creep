91fbbd605a666f5758f382b9f9e89566517ce7c0
Balthasar Schüss <mail@bschuess.dev>
2021-01-05 11:07:06 +0100

    Fix slurm scripts

--- Slurm/grid.sh
+++ Slurm/grid.sh
@@ -11,7 +11,7 @@ for a in "${!alphas[@]}"; do
   for g in "${!gammas[@]}"; do
     for e in "${!entropy[@]}"; do
       echo Starting job with "${alphas[a]}" "${gammas[g]}" "${entropy[e]}"
-      /bin/bash -c "sbatch --partition=All start.sh $index ${alphas[$a]} ${gammas[$g]} ${entropy[$e]}"
+      sbatch --partition=All start.sh $index "${alphas[$a]}" "${gammas[$g]}" "${entropy[$e]}"
       index=$((index+1))
     done
   done
