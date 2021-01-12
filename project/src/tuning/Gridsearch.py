0ab3af77383bf247588a6bc93c721c7d66c8d213
Balthasar Schüss <mail@bschuess.dev>
2021-01-12 12:32:43 +0100

    Prepare slurm for grid search

--- project/src/tuning/Gridsearch.py
+++ project/src/tuning/Gridsearch.py
@@ -1,5 +1,5 @@
 from itertools import product
-from ..agents.a2c import A2CLearner
+
 class Gridsearch():
 
     def __init__(self, executor, agent_class, **kwargs):
@@ -9,9 +9,11 @@ class Gridsearch():
     def run(self):
         hyperparams = {
                 "alpha":[0.001],
-                "gamma":[0.999, 0.995, 0.9999,0.99995, 0.99999],
+                "gamma":[0.99, 0.999, 0.995, 0.9999,0.99995, 0.99999],
                 "entropy_beta":[1,0.75,0.5,0.25,0.125,0.075],
-                "entropy_fall":[0.9999,0.999,0.99,0.95,0.9]
+                "entropy_fall":[1, 0.99999, 0.9999,0.999],
+                #"advantage": ["3step", "a2c", "td", "reinforce"],
+                #"network" : ["split", "multihead"],
         }
         keys = [*hyperparams.keys()]
         recombinations = product(*[hyperparams[key] for key in keys])
