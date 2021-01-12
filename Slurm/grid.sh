98c33ed8188ff2c40f9f6716aa97b05b62798090
Balthasar Schüss <bschuess@yahoo.de>
2021-01-12 04:05:07 +0100

    Extendend run scripts

--- Slurm/grid.sh
+++ Slurm/grid.sh
@@ -2,17 +2,24 @@
 alphas=(0.001)
 gammas=(0.999 0.9999 0.99999)
 entropy=(0.9 0.5 0.1 0.01 0.001)
+entropy_fall=(1.0)
 
 len_g="${#gammas[@]}"
 len_e="${#entropy[@]}"
 
 index=$1
+net=$2
+adv=$3
+dir=$4
+
 for a in "${!alphas[@]}"; do
   for g in "${!gammas[@]}"; do
     for e in "${!entropy[@]}"; do
-      echo Starting job with "${alphas[a]}" "${gammas[g]}" "${entropy[e]}"
-      sbatch --partition=All start.sh $index "${alphas[$a]}" "${gammas[$g]}" "${entropy[$e]}"
-      index=$((index+1))
+      for f in "${!entropy_fall[@]}"; do
+        echo Starting job with "${alphas[a]}" "${gammas[g]}" "${entropy[e]}"
+        sbatch --partition=All start.sh $index "${alphas[$a]}" "${gammas[$g]}" "${entropy[$e]}" "${entropy_fall[$f]}" "$net" "$adv" "$dir"  
+        index=$((index+1))
+      done
     done
   done
 done
--- Slurm/start.sh
+++ Slurm/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ../project
-python3 -m src.worm.train -n 10000  -w "$1" -a "$2" -g "$3" -e "$4" -ef 1 -r worm_results -s 1000
+python3 -m src.worm.train a2c -n 7500 -t 50 -w "$1" -a "$2" -g "$3" -e "$4" -ef "$5" -net "$6" -adv "$7" -r "$8" -s 1000
 
