181b229d6e3533df6fa3a7b625adb409beae1081
Denny Steigmeier <steigmeier@lam.cip.ifi.lmu.de>
2021-01-16 09:39:14 +0100

    fix bug in var name

--- Slurm/batch_grid_start.sh
+++ Slurm/batch_grid_start.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-advs=(3step, a2c, td, reinforce)
+adv=(3step, a2c, td, reinforce)
 network=(split multihead)
-index=$1
-parallel=$2
-dir=$3
+index=0
+parallel=0
+dir=ball_test_denny
 errorbuffer=15
 
 for a in "${!adv[@]}"; do
-  for g in "${!network[@]}"; do
+    for g in "${!network[@]}"; do
         echo Starting job with "${adv[a]}" "${network[g]}" 
         sbatch --partition=All grid_start.sh $index "${adv[$a]}" "${network[$g]}" "$parallel" "$dir" 
         index=$((index + parallel + errorbuffer))
   done
 done
-
 
