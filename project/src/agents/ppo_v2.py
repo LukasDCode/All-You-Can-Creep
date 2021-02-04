d4cb317bd6ad5e6a35faeb9911b4613f41904ac4
Denny <denny.steigmeier@campus.lmu.de>
2021-02-04 13:12:56 +0100

    ppo_v2 policy implemented

--- project/src/agents/ppo_v2.py
+++ project/src/agents/ppo_v2.py
@@ -60,7 +60,7 @@ class ActorNetwork(nn.Module):
                 nn.Linear(fc1_dims, fc2_dims),
                 nn.ReLU(),
         )
-        self.action_head_loc = nn.Sequential()(
+        self.action_head_loc = nn.Sequential(
             nn.Linear(fc2_dims, n_actions),
             nn.Tanh(),
         )
@@ -118,6 +118,9 @@ DEFAULT_ALPHA_CRITIC = 0.0003
 DEFAULT_BATCH_SIZE = 2024
 DEFAULT_BUFFER_SIZE = 20240
 DEFAULT_EPOCHS = 3
+DEFAULT_GAMMA = 0.995
+DEFAULT_GAE_LAMBDA = 0.95
+DEFAULT_POLICY_CLIP = 0.2
 
 DEFAULT_HIDDEN_ACTOR = 512
 DEFAULT_HIDDEN_CRITIC = 512
@@ -127,12 +130,6 @@ class PPOv2Learner(Agent):
     @staticmethod
     def agent_name():
         return "PPOv2"
-
-    @staticmethod
-    def add_config_args(parser):
-        parser.add_argument('-ha', '--hidden_actor', type=int, default=DEFAULT_HIDDEN_ACTOR, help='hidden layer units actor NN')
-        parser.add_argument('-hc', '--hidden_critic', type=int, default=DEFAULT_HIDDEN_CRITIC, help='hidden layer units critic NN')
-        return parser
     
     @staticmethod
     def add_hyper_param_args(parser):
@@ -141,12 +138,67 @@ class PPOv2Learner(Agent):
         parser.add_argument('-bas', '--batch_size', type=int, default=DEFAULT_BATCH_SIZE, help='Batch size')
         parser.add_argument('-bus', '--buffer_size', type=int, default=DEFAULT_BUFFER_SIZE, help='Buffer size, should be multiple batch size')
         parser.add_argument('-epochs','--n_epochs', type=int, default=DEFAULT_EPOCHS, help='Epochs of updates')
+        parser.add_argument('-g','--gamma', type=float, default=DEFAULT_GAMMA, help='Discount factor for rewards')
+        parser.add_argument('-gae','--gae_lambda', type=float, default=DEFAULT_GAE_LAMBDA, help='TODO')
+        parser.add_argument('-clip','--policy_clip', type=float, default=DEFAULT_POLICY_CLIP, help='TODO')
         return parser
 
-    def __init__(self, env, **kwargs):
+    @staticmethod
+    def add_config_args(parser):
+        parser.add_argument('-ha', '--hidden_actor', type=int, default=DEFAULT_HIDDEN_ACTOR, help='hidden layer units actor NN')
+        parser.add_argument('-hc', '--hidden_critic', type=int, default=DEFAULT_HIDDEN_CRITIC, help='hidden layer units critic NN')
+        return parser
+
+    def __init__(
+
+        self,
+        env,
+
+        # hyper params
+        alpha_actor = DEFAULT_ALPHA_ACTOR,
+        alpha_critic = DEFAULT_ALPHA_CRITIC,
+        batch_size = DEFAULT_BATCH_SIZE,
+        buffer_size = DEFAULT_BUFFER_SIZE,
+        n_epochs = DEFAULT_EPOCHS,
+        gamma = DEFAULT_GAMMA,
+        gae_lambda = DEFAULT_GAE_LAMBDA,
+        policy_clip = DEFAULT_POLICY_CLIP,
+
+        # config params
+        hidden_actor=DEFAULT_HIDDEN_ACTOR,
+        hidden_critic=DEFAULT_HIDDEN_CRITIC,
+
+        **kwargs):
+
         super().__init__(env)
 
+        # (self, nr_actions, input_dims, alpha, fc1_dims=512, fc2_dims=512, chkpt_dir='tmp/ppo'):
+        self.actor = ActorNetwork(self.nr_actions, self.nr_input_features, alpha_actor, fc1_dims=hidden_actor, fc2_dims=hidden_actor)
+        # (self, input_dims, alpha, fc1_dims=512, fc2_dims=512, chkpt_dir='tmp/ppo'):
+        self.critic = CriticNetwork(self.nr_input_features, alpha_critic, fc1_dims=hidden_critic, fc2_dims=hidden_critic)
+        self.memory = PPOMemory(batch_size)
+
+        self.gamma = gamma
+        self.policy_clip = policy_clip
+        self.n_epochs = n_epochs
+        self.gae_lambda = gae_lambda
+
     def policy(self, state):
-        return np.random.uniform(-1, 1, self.nr_actions)
+
+        state = T.tensor([state], dtype=T.float).to(self.actor.device)
+
+        locs, scales = self.actor(state)
+        value = self.critic(state)
+
+        dist = T.distributions.normal.Normal(locs, scales)
+        action = dist.sample()
+        
+        probs = T.squeeze(dist.log_prob(action)).tolist()
+        action = T.squeeze(action).tolist()
+        value = T.squeeze(value).item()
+
+        return action, probs, value
+
+        #return np.random.uniform(-1, 1, self.nr_actions)
         
     
